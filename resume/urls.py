69e9e28d8489941225760bd2b685637203086530
Khnissi Amen Allah <61561817+amenkhnissi@users.noreply.github.com>
2024-07-19 16:27:06 +0300

    Delete resume directory

--- resume/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path 
-urlpatterns = [
-    path('resume',views.resume_home,name='home_page'),
-    
-]
--- resume/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def resume_home (request):
-    return render (request,'index.html',{'title':'home_page'})
